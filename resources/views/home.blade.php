639837355476b94d5ddc7b61918807ec0ff226e4
Sóstenes <sostenes@novomundo.com.br>
2018-09-24 17:24:18 -0300

    pacote de teste para deploy

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<doctype <!DOCTYPE html>
+<html>
+ <body>
+    <a href="itms-services://?action=download-manifest&url=https://cockpith.novomundo.com.br/ios/manifest.plist">Install iOS in-house App</a>
+    <br><br>
+    API Version {!! \Tremby\LaravelGitVersion\GitVersionHelper::getVersion(); !!}
+ </body>
+</html>

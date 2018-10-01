326d3932ffc1a65c9f0bf59a8e4a9ccbf2a36bb2
Sóstenes <sostenes@novomundo.com.br>
2018-10-01 12:30:52 -0300

    tutorial de instalação do iOS

---
+++ resources/views/ios-tutorial.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cockpit Novo Mundo</title>
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/app.css">
+ </head>
+ <body>
+    <div id="content">
+        <p>Para instalar o <b>Cockpit</b> em seu dispositivo iOS (Iphone, Ipad, etc...) siga o seguintes passos.</p>
+        
+        <p><b>1. - </b> Em seu dispositivo iOS, abra o navegador (Safari, Chrome) e acesse o site <b>cockpit.novomundo.com.br</b>. Conforme imagem abaixo.</p>
+
+        <div id="left">
+            <img src="/img/ios-tutorial/1.png" alt="">            
+        </div>
+
+        <div>&nbsp;</div>
+        <hr>
+
+        <p><b>2. - </b> Clique na opção do iOS e depois em <b>Instalar</b>, conforme imagem abaixo.</p>
+
+        <div id="left">
+            <img src="/img/ios-tutorial/2.png" alt="">            
+        </div>
+
+        <div>&nbsp;</div>
+        <hr>
+
+        <p><b>3. - </b> Clique no Botão <b>"Home"</b> e volte a visualização de seus aplicativos. Aguarde o fim da instalação e você verá o ícone do <b>Cockpit</b>, conforme imagem abaixo.</p>
+
+        <div id="left">
+            <img src="/img/ios-tutorial/3.png" alt="">            
+        </div>
+
+        <div>&nbsp;</div>
+        <hr>
+
+        <p><b>4. - </b> Clique no ícone do Cockpit para abrir o aplicativo. Você verá uma mensagem <b>"Desenvolvedor Empresarial Não Confiável..."</b>, clique em <b>Cancelar</b>, conforme imagem abaixo.</p>
+
+        <div id="left">
+            <img src="/img/ios-tutorial/4.png" alt="">            
+        </div>
+
+        <div>&nbsp;</div>
+        <hr>
+            
+        <p><b>5. - </b> Será necessário confiar em nosso desenvolvedor enterprise, para isso, entre em: <b>Ajustes</b> > <b>Geral</b> > <b>Gerenciamento do Dispositivo</b>. Você verá conforme a imagem abaixo, então clique em <b>"Novo Mundo Móveis e Utilidades..."</b></p>
+
+        <div id="left">
+            <img src="/img/ios-tutorial/5.png" alt="">            
+        </div>
+
+        <div>&nbsp;</div>
+        <hr>
+
+        <p><b>6. - </b> Você verá conforme a imagem abaixo, clique em <b>Confiar em "Novo Mundo Móveis e Utilidad..." </b></p>
+
+        <div id="left">
+            <img src="/img/ios-tutorial/6.png" alt="">            
+        </div>
+
+        <div>&nbsp;</div>
+        <hr>
+
+        <p><b>7. - </b> Você verá conforme a imagem abaixo, clique na opção <b>Confiar</b></p>
+
+        <div id="left">
+            <img src="/img/ios-tutorial/7.png" alt="">            
+        </div>
+
+        <div>&nbsp;</div>
+        <hr>
+
+        <p><b>8. - </b> Pronto! O aplicativo está pronto para uso, volte até a lista de aplicativos e clique no ícone do aplicativo <b>Cockpit</b>, 
+        você verá conforme a imagem abaixo, utilize seu <b>Re</b> e <b>Senha</b> (a mesma do vtrine) para entrar no aplicativo.</p>
+
+        <div id="left">
+            <img src="/img/ios-tutorial/8.png" alt="">            
+        </div>
+
+        <div>&nbsp;</div>
+        <hr>
+       
+    </div>
+    
+ </body>
+</html>
